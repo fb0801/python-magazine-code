94b09d498c19ade099f6872bce894ba22b6faf41
Farhan <74499472+fb0801@users.noreply.github.com>
2021-11-02 14:59:51 +0000

    Add files via upload

---
+++ digitalclock.py
@@ -0,0 +1,26 @@
+#make a python digital clock
+
+#imprt our modules
+import time
+import tkinter as tk
+
+
+def tick(time=""):
+    #get the current time
+    time2 = time.strftime('%h:%m:%s') #format for the time
+    if time2 != time1:
+        time1 = time2
+        clock.config(text=time2)
+
+    clock.config(text=time2)
+
+root=tk.Tk()
+#create a label for the clock
+clock=tk.Label(root,font=('arial',20,'bold'), bg='green')
+
+#placement of the clock
+clock.pack(fill='both',expand=1)
+
+
+tick()#call the tick func
+root.mainloop()
