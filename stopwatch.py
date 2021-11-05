cb0dd87eb60b6edae7d91f4ef242eb96b2ab55c9
Farhan <74499472+fb0801@users.noreply.github.com>
2021-11-05 21:47:39 +0000

    Add files via upload

--- stopwatch.py
+++ stopwatch.py
@@ -7,4 +7,9 @@ import tkinter
 
 class StopWatch(tkinter.Frame):
     @classmethod
-    def 
+    def main(cls):
+        tkinter.nodefaultroot()
+        root=tkinter.Tk()
+        root.title('stopwatch')#title of the tkinter widget
+        root.resizeable(true, false) #change window size
+        root.grid
